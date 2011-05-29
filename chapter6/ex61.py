d4dc2110752e1f344fed6bd50d229d53b5ae3e7e
Mark Hutchison <mark.d.hutchison@gmail.com>
2011-05-28 21:57:35 -0700

    Ch. 6 complete

---
+++ chapter6/ex61.py
@@ -0,0 +1,7 @@
+﻿# ex 6.1 Python :: Mark Hutchison
+word = 'python'
+idx = len(word) - 1
+while idx >= 0 :
+	letter = word[idx]
+	print letter
+	idx = idx - 1
---
+++ chapter6/ex65.py
@@ -0,0 +1,5 @@
+﻿# ex 6.5 Python :: Mark Hutchison
+str = 'X-DSPAM-Confidence: 0.8475'
+startpos = str.find(':')
+newNUM = float( str[startpos+1:].strip() )
+print newNUM

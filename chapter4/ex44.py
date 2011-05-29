180196952bbfc33cd1e5c30ed0bfa53e075e7350
Mark Hutchison <mark.d.hutchison@gmail.com>
2011-05-28 20:20:05 -0700

    added chapter 4 exercises

---
+++ chapter4/ex44.py
@@ -0,0 +1,25 @@
+﻿# ex 4.4 Python :: Mark Hutchison
+def computepay(h, r):
+	try:
+		hrs = float(h)
+		rate = float(r)
+	except:
+		print 'Oops! I need a number.'
+		return
+	if hrs > 40.0 :
+		basepay = 40.0 * rate
+		OThrs = hrs - 40.0
+		OTrate = rate * 1.5
+		OTpay = OTrate * OThrs
+		pay = basepay + OTpay
+	else :
+		pay = hrs * rate
+	print 'Hours: ' + str(hrs)
+	print 'Rate: ' + str(rate)
+	print 'Your pay: ' + str(round(pay,2))
+
+computepay(18, 19)
+computepay(8.5, 10.25)
+computepay('frog', 'toad')
+computepay(50, 10)
+
---
+++ chapter4/ex45.py
@@ -0,0 +1,30 @@
+﻿# ex 4.5 Python :: Mark Hutchison
+def computegrade(g):
+	try:
+		score = round(float(g),2)
+		if score >= 0.0 and score <= 1.0 :
+			if score > 0.9 :
+				print 'A'
+			elif score > 0.8 :
+				print 'B'
+			elif score > 0.7 :
+				print 'C'
+			elif score > 0.6 :
+				print 'D'
+			elif score <= 0.6 :
+				print 'F'
+		else :
+			print 'Score must be between 0.0 and 1.0'
+	except:
+		print 'Score must be a number.'
+
+computegrade(0.95)
+computegrade(0.85)
+computegrade(0.75)
+computegrade(0.65)
+computegrade(0.5)
+computegrade('dire')
+computegrade(55)
+
+
+
